643bd2995d6a6c3ca89d064dba39935bfb471232
Chan Nundoo <chan.nundoo@gmail.com>
2023-08-09 11:41:37 +0100

    HN-1: Translating response to desired output from requirements

--- HackerNewsGateway/Program.cs
+++ HackerNewsGateway/Program.cs
@@ -1,4 +1,5 @@
 using HackerNewsGateway.Model;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,18 +13,14 @@ var app = builder.Build();
 app.MapGet("/stories/best/{n}", async (IHttpClientFactory httpClientFactory, int n) =>
 {
     if (n <= 0)
-    {
         return Results.BadRequest("Invalid value for n.");
-    }
 
     var httpClient = httpClientFactory.CreateClient();
     var hackerNewsApiUrl = "https://hacker-news.firebaseio.com/v0/beststories.json";
 
     var bestStoryIds = await httpClient.GetFromJsonAsync<List<int>>(hackerNewsApiUrl);
     if (bestStoryIds == null || bestStoryIds.Count == 0)
-    {
         return Results.NotFound("No best stories found.");
-    }
 
     var bestStories = new List<HackerNewsItem>();
     foreach (var storyId in bestStoryIds)
@@ -36,7 +33,19 @@ app.MapGet("/stories/best/{n}", async (IHttpClientFactory httpClientFactory, int
         }
     }
 
-    var selectedBestStories = bestStories.OrderByDescending(b => b.Score).Take(n);
+    var selectedBestStories = bestStories
+        .OrderByDescending(b => b.Score)
+        .Take(n)
+        .Select(p =>
+            new 
+            {
+                Title = p.Title,
+                Uri = p.Url,
+                PostedBy = p.By,
+                Time = new DateTime(p.Time, DateTimeKind.Utc),
+                Score = p.Score,
+                CommentCount = p.Kids.Length
+            });
 
     return Results.Ok(selectedBestStories);
 });
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# DeveloperCodingTest
+# Developer Coding Test - Hacker News API
+
+## Documentation
+
+* Open the solution in VS 2022 and press F5 to launch the API
+* Default url is currently set to `http://localhost:5038/stories/best/1`
+* To retrieve top n stories retrieved, change the URL parameter from 1 to n
